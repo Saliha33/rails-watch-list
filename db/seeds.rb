0ccde3a7d246bf96bac251a167906b8e8feeb3b1
saliha laidi <saliha.laidi@hotmail.com>
2022-11-18 16:47:26 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# puts 'Creating 15 fake movies...'
+# 15.times do
+#   movie = Movie.new(
+#     name: Faker::Movie.title,
+#     overview: Faker::Address.full_address,
+#     poster_url: Faker::Mountain.range,
+#     rating: rand(50...150),
+#   )
+#   movie.save!
+# end
+# puts 'Finished!'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = URI.open(url).read
+movies = JSON.parse(response)
+movies['results'].each do |movie|
+  # byebug
+  film = Movie.new(
+        title: movie['title'],
+        overview: movie['overview'],
+        poster_url: movie['poster_url'],
+        rating: movie['vote_average']
+      )
+      film.save!
+end
